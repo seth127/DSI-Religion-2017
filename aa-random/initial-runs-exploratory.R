506b2e98b6cb141ebccb2b28bcf711dbc6c21732
Seth Green <seth127@gmail.com>
2016-11-30 10:01:55 -0500

    some exploratory

--- aa-random/initial-runs-exploratory.R
+++ aa-random/initial-runs-exploratory.R
@@ -2,7 +2,7 @@ setwd("~/Documents/DSI/Capstone/DSI-Religion-2017")
 library(ggplot2)
 library(dplyr)
 
-df <- read.csv('modelOutput/modelStats.csv', stringsAsFactors = F)
+df <- read.csv('modelOutput/modelStats.csv', stringsAsFactors = T)
 View(df)
 
 judgeDF <- group_by(df, judgementMethod)
@@ -11,25 +11,20 @@ names(judgeAcc) <- c("meth", "rf", "svm")
 
 ggplot(data=judgeAcc, aes(x=meth, y=rf)) + geom_bar(stat="identity", position=position_dodge())
 
-# avgSD for 
-ggplot(df, aes(x=runID, fill=judgementMethod, y=rfAccuracy)) + geom_bar(stat="identity", position=position_dodge())
-
-
-orderDF <- function(df, col, fac) {
-  # order it
-  df <- df[order(df$col, decreasing=T), ]
-  rownames(df) <- NULL
-  # set factor levels to new order
-  df <- within(df, fac <- factor(fac, levels=fac))
-  df
-}
-
+# SVM ACCURACY BY JUDGEMENT METHOD
 # order it
-df <- df[order(df$col, decreasing=T), ]
+df <- df[order(df$svmAccuracy, decreasing=T), ]
 rownames(df) <- NULL
-# set factor levels to new order
-df <- within(df, fac <- factor(fac, levels=fac))
-df
+# plot it
+# with PRONOUN COUNTS
+ggplot(df[!is.na(df$nous),], aes(x=as.numeric(row.names(df[!is.na(df$nous),])), fill=judgementMethod, y=svmAccuracy)) + geom_bar(stat="identity", position=position_dodge())
+
+# without PRONOUN COUNTS
+ggplot(df[is.na(df$nous),], aes(x=as.numeric(row.names(df[is.na(df$nous),])), fill=judgementMethod, y=svmAccuracy)) + geom_bar(stat="identity", position=position_dodge())
+
+
+
+
   # now plot it
   #ggplot(df, aes(x=fac, y= col, fill=ClassGeneral)) + geom_bar(stat="identity", position=position_dodge()) + ggtitle("Percentage of readings with Screen On") + 
     #scale_y_continuous(breaks = c(.05,.1,.15,.2), labels = c("5%", "10%", "15%", "20%"))
