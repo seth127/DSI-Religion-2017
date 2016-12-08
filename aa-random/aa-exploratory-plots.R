42c3bccd68af0fd9c0e46547f2660155f1c9501f
Seth Green <seth127@gmail.com>
2016-12-08 10:26:20 -0500

    some graphics

--- aa-random/aa-exploratory-plots.R
+++ aa-random/aa-exploratory-plots.R
@@ -146,10 +146,14 @@ for (file in files) {
 }
 dim(signalDF)
 
+# KEYWORD + PRONOUN JUDGEMENTS
 ggplot(signalDF, aes(x=rank, y=PSJudge, colour = groupName)) + geom_point(size=5) + ggtitle("Keyword + Pronoun Judgements")
-
 ggplot(signalDF, aes(x=rank, y=PSJudge, colour = groupName)) + geom_boxplot() + ggtitle("Keyword + Pronoun Judgements")
 ggplot(signalDF, aes(x=as.factor(rank), y=PSJudge)) + geom_boxplot() + xlab("rank") + ggtitle("Keyword + Pronoun Judgements")
+
+# AVGSD 
+ggplot(signalDF, aes(x=rank, y=avgSD, colour = groupName)) + geom_boxplot() + ggtitle("Keyword + Pronoun Judgements")
+
 
 # by number of keywords selected
 for (i in 1:nrow(signalDF)){
@@ -158,4 +162,50 @@ for (i in 1:nrow(signalDF)){
   signalDF$keywordCount[i] <- length(keys)
 }
 ggplot(signalDF, aes(x=keywordCount, y=PSJudge, colour = rank)) + geom_point(size=5) + ggtitle("PSJudge")
+
+ggplot(signalDF, aes(x=keywordCount, y=avgSD, colour = rank)) + geom_point(size=5) + ggtitle("avgSD")
+ggplot(signalDF, aes(x=as.factor(keywordCount), y=avgSD, colour = groupName)) + geom_boxplot() + ggtitle("avgSD")
+
+#
+# with PSJUDGE vs pronouns
+vip = ggplot(df[!is.na(df$PSJudge),], aes(x=nous, y=svmAccuracy)) + geom_point(size=5, aes(colour='nous')) + ggtitle('') + xlab('variable importance')
+vip = vip + geom_point(size=5, aes(x=ils, y=svmAccuracy, colour = 'ils'))
+vip = vip + geom_point(size=5, aes(x=nous, y=svmAccuracy, colour = 'nous'))
+vip = vip + geom_point(size=5, aes(x=PSJudge, y=svmAccuracy, colour = 'PSJudge'))
+vip = vip + geom_point(size=5, aes(x=pronounFrac, y=svmAccuracy, colour = 'pronounFrac'))
+#vip = vip + geom_point(size=5, aes(x=avgSD, y=svmAccuracy, colour = 'avgSD'))
+#vip = vip + geom_point(size=5, aes(x=avgEVC, y=svmAccuracy, colour = 'avgEVC'))
+vip
+
+###################
+#   with toBe
+##################
+files <- c("modelOutput/modelPredictions-coco_3_cv_3_netAng_30_twc_15_tfidfNoPro_both_bin_10-RO610D.csv",
+           "modelOutput/modelPredictions-coco_3_cv_3_netAng_30_twc_20_tfidfNoPro_both_bin_10-2YZAOL.csv"
+           )
+
+signalDF <- read.csv(files[1], stringsAsFactors = T)
+for (i in 2:length(files)) {
+  newDF <- read.csv(files[i], stringsAsFactors = T)
+  signalDF <- rbind(signalDF, newDF)
+  print(files[i])
+  print(dim(newDF))
+}
+dim(signalDF)
+
+ggplot(signalDF, aes(x=rank, y=toBeFrac, colour = groupName)) + geom_boxplot() + ggtitle("noun + 'to be' + adj Judgements")
+
+###################
+#   scores
+##################
+
+df$id <- as.numeric(row.names(df))
+rs1 <- data.frame(id = df$id, Accuracy = df$svmAccuracy, Model = rep("SVM", nrow(df)))
+rs2 <- data.frame(id = df$id, Accuracy = df$rfAccuracy, Model = rep("Random Forest", nrow(df)))
+rsdf <- rbind(rs1, rs2)
+rsdf$Accuracy <- rsdf$Accuracy * 100
+accplot <- ggplot(rsdf[rsdf$id < 60 | rsdf$id > 66 ,], aes(x=id, y=Accuracy, colour = Model)) + geom_line() + ggtitle("Model Accuracy") + xlab('Run ID Number')
+accplot <- accplot + geom_hline(yintercept = 84, linetype = "longdash") 
+#accplot <- accplot + geom_vline(xintercept = 66, linetype = "longdash") 
+accplot
 
