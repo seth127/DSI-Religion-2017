6a2477663e567b6e446f520b7334e626e1cd6ae8
Megan Stiles <megan.e.stiles@gmail.com>
2017-01-24 12:20:05 -0500

    added script to make dataframe for all single doc predicitons

---
+++ Directory Maintence/organizeoutputfiles.py
@@ -0,0 +1,20 @@
+import os
+
+os.chdir('./Desktop/github/DSI-Religion-2017/')
+
+files = os.listdir('./modelOutputSingleDocs/logs/')
+
+predictions = []
+for file in files:
+    if 'modelPredictions' in file:
+        predictions.append(file)
+
+model_predictions = pd.DataFrame()
+
+os.chdir('/Users/meganstiles/Desktop/github/DSI-Religion-2017/modelOutputSingleDocs/logs')
+
+for file in predictions:
+    data = pd.read_csv(file)
+    model_predictions = model_predictions.append(data)
+
+model_predictions.to_csv('All SingleDoc Runs.csv')
