2ad0ee5ff2cf58b23f939e8dbcdddb6d96a03a03
Megan Stiles <megan.e.stiles@gmail.com>
2017-03-23 12:19:48 -0400

    update signal script

--- signalFiles/CleanSignals.py
+++ signalFiles/CleanSignals.py
@@ -2,9 +2,9 @@ import os
 import pandas as pd
 import re
 
-os.chdir('/Users/meganstiles/Desktop/github/DSI-Religion-2017/modelOutputSingleDocs/logs/')
+os.chdir('/Users/meganstiles/Desktop/github/DSI-Religion-2017/modelOutput/logs/')
 
-signals = pd.read_csv('signalOutput-coco_3_cv_3_netAng_30_twc_10_tfidfNoPro_pronoun_bin_1-GK7UT6.csv')
+signals = pd.read_csv('signalOutput-coco_3_cv_3_netAng_30_twc_10_tfidfNoPro_pronoun_bin_10-MR8VUU.csv')
 
 signals = signals.drop(signals.columns [[0,2,3,4,]], axis=1)
 
@@ -18,7 +18,7 @@ for i in range(0,len(signals)):
 
 #Read in Rank filte
 os.chdir('/Users/meganstiles/Desktop/github/DSI-Religion-2017/refData/')
-ranks = pd.read_csv('docRanks.csv') 
+ranks = pd.read_csv('groupRanks.csv') 
 
 #for i in range(0,len(ranks)):
  #   name = ranks['groupName'][i]
---
+++ signalFiles/cleanSignalsBinned.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+import re
+
+os.chdir('/Users/meganstiles/Desktop/github/DSI-Religion-2017/modelOutput/logs/')
+
+signals = pd.read_csv('signalOutput-coco_3_cv_3_netAng_30_twc_10_tfidfNoPro_pronoun_bin_10-E5XCKL.csv')
+
+signals = signals.drop(signals.columns [[0,2,3,4,]], axis=1)
+
+
+#Read in Rank filte
+os.chdir('/Users/meganstiles/Desktop/github/DSI-Religion-2017/refData/')
+ranks = pd.read_csv('groupRanksBinned.csv') 
+
+
+
+#Remove test/train from GroupID
+for i in range(0,len(signals)):
+    name = signals['groupId'][i]
+    name = re.sub('_train[0-9]*', '', str(name))
+    name = re.sub('_test[0-9]*', '', name)
+    signals['groupId'][i] = name
+
+#Merge Group Ranks
+clean = pd.merge(signals, ranks, how = 'inner', on='groupId') 
+
+#Change Dir 
+
+os.chdir('/Users/meganstiles/Desktop/github/DSI-Religion-2017/signalFiles/')
+
+clean.to_csv('binnedSignals3.csv')
+
+
+
