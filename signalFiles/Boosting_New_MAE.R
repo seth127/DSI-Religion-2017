6e03fdea8273160f41d86ae78eefcf2b9bda0a10
Megan Stiles <megan.e.stiles@gmail.com>
2017-04-19 11:49:27 -0400

    added Mean Absolute Error

---
+++ signalFiles/Boosting_New_MAE.R
@@ -0,0 +1,116 @@
+#Read In Files
+
+setwd('/Users/meganstiles/Desktop/github/DSI-Religion-2017/signalFiles/')
+
+#gradient Boosting
+require(xgboost)
+library(caret)
+library(dplyr)
+df_clean = read.csv('SingleDocSignals.csv')
+#Reset Rank Levels, for xgboost in multiclass classification, the classes are (0, num_class) so we subtract one from rank
+df_clean$rank<- df_clean$rank - 1
+
+#Set Rank as Factor
+df_clean$rank<- as.factor(df_clean$rank)
+
+#Drop Unneeded Variables:
+df_clean<- df_clean[,-c(1)]
+#10 fold CV
+
+#Set Seed
+set.seed(21)
+#Create Folds
+folds<- createFolds(df_clean$rank, k=10, list = TRUE, returnTrain = FALSE)
+
+param <- list("objective" = "multi:softprob",    
+              "num_class" = 9,
+              'max_depth' = 5,
+              'eval_metric' = 'merror')
+
+
+#initialzie empty vector to store accuracy
+raw_accuracy<- vector()
+difference<- vector()
+i=0
+j=0
+for (i in 1:10) {
+  #Create testing indicies based on folds
+  test.indices<- folds[[i]]
+  
+  #Create training and testing sets
+  train = df_clean[-test.indices,]
+  test = df_clean[test.indices,]
+  
+  #Convert to Matrix
+  train_X<-data.matrix(train[,c(1:18, 20)])
+  train_Y<- data.matrix(train$rank)
+  test_X<- data.matrix(test[,c(1:18, 20)])
+  test_Y = data.matrix(test$rank)
+  
+  #train Model
+  model <- xgboost(param=param, data=train_X, label=train_Y, nrounds=5)
+  
+  #Make predictions based on model for testing set
+  predictions<- predict(model, test_X)
+  
+  # reshape it to a num_class-columns matrix
+  pred <- matrix(predictions, ncol=9, byrow=TRUE)
+  
+  # convert the probabilities to softmax labels (we have to subtract  one)
+  pred_labels <- max.col(pred) - 1
+  
+  #Find the difference between the predicted value and the actual value
+  for (j in 1: length(test_Y)) {
+    diff<- abs(as.numeric(test_Y[j]) - pred_labels[j])
+    difference[j]<- diff
+  }
+  
+  #Calculate Aean Absolute Error
+  accuracy<-mean(difference)
+  
+  #Store accuracy for each run in vector
+  raw_accuracy[i]= accuracy
+}
+
+Avg_Accuracy = mean(raw_accuracy)
+Avg_Accuracy #MAE = 1.08393
+
+####################################
+#Get Predictions for all Documents
+###################################
+All_Docs<- read.csv('ALLDOCSIGNALS.csv')
+
+#Drop Unneeded Column
+
+All_Docs<- All_Docs[, -1]
+
+#Creating Training and Prediction Matricies
+
+train_X<-data.matrix(df_clean[,c(1:18, 20)])
+train_Y<- data.matrix(df_clean$rank)
+test_X<- data.matrix(All_Docs)
+
+
+#Set Model Parameters
+param <- list("objective" = "multi:softprob",    
+              "num_class" = 9,
+              'max_depth' = 5,
+              'eval_metric' = 'merror')
+#train Model
+model <- xgboost(param=param, data=train_X, label=train_Y, nrounds=5)
+
+#Make predictions based on model for testing set
+predictions<- predict(model, test_X)
+
+# reshape it to a num_class-columns matrix
+pred <- matrix(predictions, ncol=9, byrow=TRUE)
+
+# convert the probabilities to softmax labels
+pred_labels <- max.col(pred) 
+
+#Bind Prediction Labels to All_Docs DF
+All_Docs<- cbind(All_Docs, pred_labels)
+
+#Write out CSV
+
+write.csv(All_Docs, file = 'AllDocsPredictions.csv')
---
+++ signalFiles/SVM_MAE.R
@@ -0,0 +1,57 @@
+
+
+###SVM##########
+
+library(kernlab)
+setwd('/Users/meganstiles/Desktop/github/DSI-Religion-2017/signalFiles/')
+
+list.files()
+
+signals<- read.csv('SingleDocSignals.csv')
+
+#Drop Unneeded Columns
+signals<- signals[,-1]
+
+#Set Rank as Factor Variable
+
+signals$rank<- as.factor(signals$rank)
+
+#Set Seed
+set.seed(21)
+
+# 10-Fold CV
+folds<-createFolds(signals$rank, k=10, list = TRUE, returnTrain = FALSE)
+
+raw_accuracy<- vector()
+difference<- vector()
+i=0
+j=0
+for (i in 1:10) {
+  #Create testing indicies based on folds
+  test.indices<- folds[[i]]
+  
+  #Create training and testing sets
+  train = signals[-test.indices,]
+  test = signals[test.indices,]
+  train$rank = factor(train$rank)
+  test$rank = factor(test$rank)
+  #train Model
+  model <- ksvm(rank ~. -groupId, data = train, type="C-svc", kernel="vanilladot", C=100)
+  
+  #Make predictions based on model for testing set
+  predictions<- predict(model, newdata = test)
+  
+  #Calculate Accuracy
+  for (j in 1: length(test)) {
+    diff<- abs(as.numeric(test$rank[j]) - as.numeric(predictions[j]))
+    difference[j]<- diff
+  }
+  
+  #Calculate Mean Absolute Error
+  accuracy<-mean(difference)
+  
+  #Store accuracy for each run in vector
+  raw_accuracy[i]= accuracy
+}
+raw_accuracy
+mean(raw_accuracy) #MAE = 1.095
